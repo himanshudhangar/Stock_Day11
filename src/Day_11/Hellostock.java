33c1b5ea77511cd0c30ced36610fb9a644fe5e0d
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-04 16:36:27 +0530

    Added main

---
+++ src/Day_11/Hellostock.java
@@ -0,0 +1,10 @@
+package Day_11;
+
+public class Hellostock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Well come stock management");
+	}
+
+}
